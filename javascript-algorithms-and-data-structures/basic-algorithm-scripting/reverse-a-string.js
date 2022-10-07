4dfa78b6ce55eca43418ba2a2a75c35c5744661e
Ciro <cbjales@gmail.com>
2022-10-06 21:33:53 -0300

    Add solution

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/reverse-a-string.js
@@ -0,0 +1,3 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/reverse-a-string
+
+const reverseString = (str) => str.split("").reverse().join("");
