d7328cc5022a341f1b41ec22c34f86583856876e
Ciro <cbjales@gmail.com>
2022-10-14 17:43:45 -0300

    Add solution

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+
+function booWho(bool) {
+  return bool === true || bool === false;
+}
