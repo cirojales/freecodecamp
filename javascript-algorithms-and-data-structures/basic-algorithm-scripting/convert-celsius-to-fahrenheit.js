02d7dec866cc5c5617090c2b8c0b49af64ab29a2
Ciro <cbjales@gmail.com>
2022-10-17 15:38:11 -0300

    chore: add solution

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/convert-celsius-to-fahrenheit.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/convert-celsius-to-fahrenheit
+
+function convertCtoF(celsius) {
+  return celsius * (9 / 5) + 32;
+}
